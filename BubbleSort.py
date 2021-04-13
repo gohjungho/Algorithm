4bce75782896e9d23827d4d362e7dc89351d0908
gohjungho <jerry940808@gmail.com>
2021-04-13 17:29:22 +0900

    21.04.13

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,12 +1,13 @@
-array = [5,3,4,1,2]
+def BubbleSort(array):
+    for i in range(len(array), 0, -1):
+        # tmp = 0 
+        for j in range(len(array) - 1, 0, -1):
+            if array[j] < array[j - 1]:
+                # tmp = array[j - 1]
+                # array[j - 1] = array[j]
+                # array[j] = tmp
+                array[j - 1], array[j] = array[j], array[j - 1]
+    return array 
 
-for i in range(len(array), 0, -1):
-    # tmp = 0 
-    for j in range(len(array) - 1, 0, -1):
-        if array[j] < array[j - 1]:
-            # tmp = array[j - 1]
-            # array[j - 1] = array[j]
-            # array[j] = tmp
-            array[j - 1], array[j] = array[j], array[j - 1]
-            
-print(array)
+array = [5,3,4,1,2]           
+print(BubbleSort(array))
