53aea5a7b0ceec762647d7acb2ff2a0828cc1c46
gohjungho <jerry940808@gmail.com>
2021-12-27 21:45:40 +0900

    code update

--- Programmers/PhoneBook.py
+++ Programmers/PhoneBook.py
@@ -1,6 +1,7 @@
 def solution(phone_book):
     phone_book = sorted(phone_book) # 11, 12, 119 -> 11, 119, 12 순으로 정렬 (string)
     for i in range(1, len(phone_book)): 
-        if phone_book[i-1] in phone_book[i]: 
+        length = len(phone_book[i-1])
+        if phone_book[i-1] in phone_book[i][:length] : 
             return False
     return True
